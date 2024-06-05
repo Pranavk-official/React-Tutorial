86202028b2cb3d360cd99b572a0cd3ea4d76f7e0
Pranav K <pranavkcse@gmail.com>
2024-06-05 13:51:12 +0530

    form handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,83 @@
-// import { UserDetails } from "./components/UserDetails";
-import { RegisterForm } from "./components/RegisterForm";
+import { useState } from "react";
+import { UserDetails } from "./components/UserDetails.jsx";
 
 export default function App() {
-  // State
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [counter, setCounter] = useState(4);
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "anson",
+      email: "anson@dev.com",
+    },
+    {
+      id: 2,
+      username: "brad",
+      email: "brad@dev.com",
+    },
+    {
+      id: 3,
+      username: "chad",
+      email: "chad@dev.com",
+    },
+  ]);
 
   return (
     <div>
-      <RegisterForm />
+      <div>
+        <h2>Add User</h2>
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          const newUser = {
+            id: counter,
+            username,
+            email,
+          };
+
+          setCounter((currentCounter) => currentCounter + 1);
+          setUsers((currentUsersState) => [...currentUsersState, newUser]);
+        }}
+      >
+        <div>
+          <div>
+            <label htmlFor="username"></label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="email"></label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <button>Add User</button>
+          </div>
+        </div>
+      </form>
+      <br />
+      <hr />
+      <br />
+      <div>
+        <h2>User Details</h2>
+      </div>
+      {users.map((user) => (
+        <UserDetails key={user.id} user={user} setUsers={setUsers} />
+      ))}
     </div>
   );
 }
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,17 +1,83 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
-export function UserDetails({ user }) {
+export function UserDetails({ user, setUsers }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [username, setUsername] = useState(user.username);
+  const [email, setEmail] = useState(user.email);
+  console.log(isEditing);
   return (
     <div>
       <b>Id: </b>
       <span>{user.id}</span>
       <br />
       <b>Username: </b>
-      <span>{user.username}</span>
+      {isEditing ? (
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+      ) : (
+        <span>{user.username}</span>
+      )}
       <br />
       <b>Email: </b>
-      <span>{user.email}</span>
+      {isEditing ? (
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      ) : (
+        <span>{user.email}</span>
+      )}
       <br />
+      <br />
+      <div>
+        <button
+          onClick={() => {
+            setIsEditing((currentState) => !currentState);
+          }}
+        >
+          Edit
+        </button>
+        <button
+          onClick={() => {
+            setUsers((currentUsersState) =>
+              currentUsersState.filter(
+                (currentUser) => currentUser.id !== user.id
+              )
+            );
+          }}
+        >
+          Delete
+        </button>
+        {isEditing && (
+          <button
+            onClick={() => {
+              setUsers((currentUsersState) => {
+                return currentUsersState.map((currentUser) => {
+                  if (currentUser.id === user.id)
+                    return { ...currentUser, username: username, email: email };
+                  else return currentUser;
+                });
+              });
+              setIsEditing(false);
+            }}
+          >
+            Save
+          </button>
+        )}
+      </div>
+      <br />
+      <hr />
       <br />
     </div>
   );
@@ -23,4 +89,6 @@ UserDetails.propTypes = {
     username: PropTypes.string.isRequired,
     email: PropTypes.string.isRequired,
   }),
+
+  setUsers: PropTypes.func.isRequired,
 };
